8f1fd95164aa0c8695ec8dc3ba387cbb835b70d5
vaporstack <andrew@vaporstack.org>
2018-12-25 15:01:06 -0800

    transform infra

---
+++ src/geo/coer_transform.c
@@ -0,0 +1,28 @@
+//
+//  coer_transform.c
+//  coer
+//
+//  Created by vs on 12/25/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#include "coer_transform.h"
+
+#include <deps/gl-matrix/gl-matrix.h>
+
+typedef struct CTransform
+{
+	vec3_t anchor;
+	vec3_t position;
+	vec3_t rotation;
+	vec3_t scale;
+	double opacity;
+	// WVec3d position;
+	// WVec3d rotation;
+	// WVec3d scale;
+	// double opacity;
+} CTransform;
+
+void coer_transform_create(CTransform* t);
+void coer_transform_destroy(CTransform* t);
+void coer_transform_reset(CTransform* t);
---
+++ src/geo/coer_transform.h
@@ -0,0 +1,14 @@
+//
+//  coer_transform.h
+//  coer
+//
+//  Created by vs on 12/25/18.
+//  Copyright © 2018 ruminant. All rights reserved.
+//
+
+#ifndef coer_transform_h
+#define coer_transform_h
+
+#include <stdio.h>
+
+#endif /* coer_transform_h */
