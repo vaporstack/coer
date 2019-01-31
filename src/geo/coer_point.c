4d864225f0188c5cffd1cf069aa548a5106185e4
vaporstack <andrew@vaporstack.org>
2019-01-31 18:14:06 -0500

    build system tweaks

---
+++ src/geo/coer_point.c
@@ -0,0 +1,9 @@
+//
+//  coer_point.c
+//  coer
+//
+//  Created by vs on 1/28/19.
+//  Copyright © 2019 ruminant. All rights reserved.
+//
+
+#include "coer_point.h"
---
+++ src/geo/coer_point.h
@@ -0,0 +1,25 @@
+//
+//  coer_point.h
+//  coer
+//
+//  Created by vs on 1/28/19.
+//  Copyright © 2019 ruminant. All rights reserved.
+//
+
+#ifndef coer_point_h
+#define coer_point_h
+
+typedef struct CPoint
+{
+	double x;
+	double y;
+} CPoint;
+
+typedef struct CPoint3
+{
+	double x;
+	double y;
+	double z;
+} CPoint3;
+
+#endif /* coer_point_h */
