325ba07bb1c191c06131b26293c3725e960f7802
vaporstack <andrew@vaporstack.org>
2019-04-26 13:53:54 -0400

    draggable

---
+++ src/anim/coer_draggable.h
@@ -0,0 +1,19 @@
+//
+//  coer_draggable.h
+//  coer
+//
+//  Created by vs on 4/24/19.
+//  Copyright © 2019 ruminant. All rights reserved.
+//
+
+#ifndef coer_draggable_h
+#define coer_draggable_h
+
+typedef struct
+{
+	CPoint start;
+	CPoint end;
+	CPoint last;
+}CDraggable;
+
+#endif /* coer_draggable_h */
